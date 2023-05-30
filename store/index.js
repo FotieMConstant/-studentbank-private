d9dcea580d8e5850d111887da3cf49f6ad92fe00
fotiemconstant <fotiemoghommahieconstant@gmail.com>
2023-05-30 14:58:38 +0300

    chore(store/index.js): comment out unused imports fix(store/index.js): set isDark state to true instead of using useDark() function chore(store/index.js): comment out unused useToggle() function call in toggleDarkAction() function

--- store/index.js
+++ store/index.js
@@ -1,9 +1,9 @@
 import { createStore } from "vuex";
-import { useDark, useToggle } from "@vueuse/core";
+// import { useDark, useToggle } from "@vueuse/core";
 
 export default createStore({
   state: {
-    isDark: useDark(), // user theme
+    isDark: true, // user theme
   },
   getters: {
     // getter for isDark from state
@@ -22,7 +22,7 @@ export default createStore({
     // toggle between light and dark mode action function
     toggleDarkAction({ commit, state }) {
       commit("setToggleDark", !state.isDark);
-      useToggle(!state.isDark);
+      // useToggle(!state.isDark);
       console.log(state.isDark);
     },
   },
